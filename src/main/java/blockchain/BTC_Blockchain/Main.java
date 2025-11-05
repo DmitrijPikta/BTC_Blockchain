4a610c6551f91b073e85dd9270b4f7c6fcef7de2
DmitrijPikta <dmitrij.pikta@gmail.com>
2025-11-05 23:43:04 +0200

    Made conversation with user implementation

--- src/main/java/blockchain/BTC_Blockchain/Main.java
+++ src/main/java/blockchain/BTC_Blockchain/Main.java
@@ -1,13 +1,30 @@
 package blockchain.BTC_Blockchain;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Blockchain blockchain = new Blockchain(1000);
-
-        blockchain.transactionGenerating(10000);
         Scanner scanner = new Scanner(System.in);
+        Blockchain blockchain = new Blockchain(1000);
+        System.out.print("How much transactions in mempool you want to generate? ");
+        int txNumberToReach;
+        while(true) {
+            try {
+                txNumberToReach = scanner.nextInt();
+                if (txNumberToReach < 0) {
+                    System.out.println("Number can not be negative. Try once again");
+                    continue;
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("It is not number. Try again");
+                scanner.nextLine();
+            }
+        }
+        scanner.nextLine();
+
+        blockchain.transactionGenerating(txNumberToReach);
         System.out.println("Press enter to continue");
         scanner.nextLine();
         blockchain.autoMining();
